b69ae78184091a1333a1f90af85a18c3f7c63195
grgshankar <shankargurung027@gmail.com>
2024-02-07 21:41:37 +0545

    fixing footer

--- inc/footer.php
+++ inc/footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="footer-up">
             <div class="row">
-                <div class="col-md-3">
+                <div class="col-lg-3">
                     <div class="footer-box">
                         <div class="footer-logo">
                             <img src="assets/img/logo.jpg" alt="footer logo">
@@ -18,56 +18,59 @@
                         </ul>
                     </div>
                 </div>
-                <div class="col-md-2">
-                    <div class="footer-box">
-                        <strong class="footer-h">Our Recent Posts</strong>
-                        <ul class="ul-post">
-                            <li><strong>Visit Thailand, Bali And China</strong>
-                                <span>October 10, 2018 at 9:06 am</span>
-                            </li>
-                            <li><strong>The Sound Of Our Jungle</strong> <span>September 7, 2016</span></li>
-                            <li>
-                                <strong>New Year, New Resolutions!</strong>
-                                <span>September 7, 2016</span>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div class="col-md-4">
-                    <div class="footer-box">
-                        <strong class="footer-h">Menus</strong>
-                        <div class="row">
-                            <div class="col-sm-12 col-md-6">
-                                <ul class="imp-link">
-                                    <li><a href="#">Home</a></li>
-                                    <li><a href="#">Destinations</a></li>
-                                    <li><a href="#">Packages</a></li>
-                                    <li><a href="#">Tours And Activities</a></li>
-                                    <li><a href="#">Blog</a></li>
-                                    <li><a href="#">About US</a></li>
-                                    <li><a href="#">Contact</a></li>
-                                </ul>
-                            </div>
-                            <div class="col-sm-12 col-md-6">
-                                <ul class="imp-link">
-                                    <li><a href="#">Privacy Policy</a></li>
-                                    <li><a href="#">Term Of Services</a></li>
-                                    <li><a href="#">Disclaimer</a></li>
-                                    <li><a href="#">Credits</a></li>
+                <div class="col-lg-9">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-6 col-lg-3">
+                            <div class="footer-box">
+                                <strong class="footer-h">Our Recent Posts</strong>
+                                <ul class="ul-post">
+                                    <li><strong>Visit Thailand, Bali And China</strong>
+                                        <span>October 10, 2018 at 9:06 am</span>
+                                    </li>
+                                    <li><strong>The Sound Of Our Jungle</strong> <span>September 7, 2016</span></li>
+                                    <li>
+                                        <strong>New Year, New Resolutions!</strong>
+                                        <span>September 7, 2016</span>
+                                    </li>
                                 </ul>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="footer-box">
-                        <strong class="footer-h">Subscribe</strong>
+                        <div class="col-sm-12 col-md-6 col-lg-3 col-xl-5">
+                            <div class="footer-box">
+                                <strong class="footer-h">Menus</strong>
+                                <div class="row">
+                                    <div class="col-sm-12 col-xl-6">
+                                        <ul class="imp-link">
+                                            <li><a href="#">Home</a></li>
+                                            <li><a href="#">Destinations</a></li>
+                                            <li><a href="#">Packages</a></li>
+                                            <li><a href="#">Tours And Activities</a></li>
+                                            <li><a href="#">Blog</a></li>
+                                            <li><a href="#">About US</a></li>
+                                            <li><a href="#">Contact</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-sm-12 col-xl-6">
+                                        <ul class="imp-link">
+                                            <li><a href="#">Privacy Policy</a></li>
+                                            <li><a href="#">Term Of Services</a></li>
+                                            <li><a href="#">Disclaimer</a></li>
+                                            <li><a href="#">Credits</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-6 col-lg-6 col-xl-4">
+                            <div class="footer-box">
+                                <strong class="footer-h">Subscribe</strong>
 
-                        <div class="footer-newletter">
-                            <input type="text" placeholder="Enter Your Email" name="mail" required>
-                            <div class="news-btn">
-                                <input type="submit" value="Subscribe">
+                                <div class="footer-newletter">
+                                    <input type="text" placeholder="Enter Your Email" name="mail" required>
+                                    <div class="news-btn">
+                                        <input type="submit" value="Subscribe">
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
